7567ae95fb84c8a5ae4c534f9f3146749720c96d
bob <haobo322@163.com>
2023-06-24 23:01:14 +0800

    添加文件

--- quickStarted/main.cpp
+++ quickStarted/main.cpp
@@ -1,13 +1,24 @@
-#include <QGuiApplication>
+#include <QApplication >
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQuickWidget>
+#include "mywidget.h"
+#include "mywidgetwrapper.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);
-
+    QApplication  app(argc, argv);
     QQmlApplicationEngine engine;
+
+    qmlRegisterType<CMyWidgetWrapper>("MyWidgetWrapper", 1, 0, "CMyWidgetWrapper");
+
+    CMyWidget *pwdgt = new CMyWidget();
+    CMyWidgetWrapper wrapper;
+    wrapper.SetWidget(pwdgt);
+    engine.rootContext()->setContextProperty("wrapper", &wrapper);
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
@@ -15,6 +26,15 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
+
+//    QQuickWidget *qmlWidget = new QQuickWidget(QUrl("qrc:/main.qml"));
+//    qmlWidget->setResizeMode(QQuickWidget::SizeRootObjectToView);
+//    qmlWidget->resize(800, 600);
+//    qmlWidget->show();
+
+////    CMyWidget *pwdgt = new CMyWidget(qmlWidget);
+////    pwdgt->resize(10,20);
+////    pwdgt->show();
 
     return app.exec();
 }
